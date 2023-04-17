30106635a9fd121aa0484f75ebeff7c1cbfdae6f
Christian <eyubechristian@gmail.com>
2023-04-17 15:11:35 +0100

    fetched data from google api

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const Search = () => {
-  return <div>Search</div>;
+const Search = async ({ searchParams }: any) => {
+  const result =
+    await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.q}
+  `);
+  const data = await result.json();
+
+  return (
+    <div>
+      {data &&
+        data.items.map((item: any) => <p key={item.title}>{item.title}</p>)}
+    </div>
+  );
 };
 
 export default Search;
